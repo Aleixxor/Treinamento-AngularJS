939962bcef9416d25778cd231603d8fc7ce289d5
Alex Louzada <alex.louzada@qualidata.com.br>
2018-11-06 17:43:44 -0200

    Commit Inicial

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var myApp = angular.module('helloworld', ['ui.router']);
+
+myApp.config(function($stateProvider) {
+  var helloState = {
+    name: 'hello',
+    url: '/hello',
+    template: `
+    <div class="col-md-12 topbar">
+        <input type="text" id="searchBar">
+    </div>`
+  }
+
+  var aboutState = {
+    name: 'about',
+    url: '/about',
+    template: '<h3>Its the UI-Router hello world app!</h3>'
+  }
+
+  $stateProvider.state(helloState);
+  $stateProvider.state(aboutState);
+});
