20d564f43d044959f2bb19d78b1dd7bb6a5dcfc1
aleixxor <alexlouzada2009@gmail.com>
2018-11-08 17:35:55 -0200

    Funcionando

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function listaMusicas($http){
   vm.previousPage = previousPage;
   vm.nextPage = nextPage;
   vm.playPreview = playPreview;
-  vm.preview;
+  vm.preview = {audio: "", url: ""};
 
   this.searchBar;
   
@@ -88,15 +88,23 @@ function listaMusicas($http){
   }
 
   function playPreview(url){
-    console.log("Preview: ");
-    console.log(url);
-    vm.preview = new Audio(url);
-    if(!vm.isPlaying){
-      vm.preview.play();
+    
+    if(vm.preview.url == url){ 
+      if(vm.isPlaying == false){
+        vm.preview.audio.play();
+      }else{
+        vm.preview.audio.pause();
+      }
+      vm.isPlaying = !vm.isPlaying;
     }else{
-      vm.preview.pause();
+      if(vm.isPlaying == true){
+        vm.preview.audio.pause();
+      }
+      vm.preview.url = url;
+      vm.preview.audio = new Audio(url);
+      vm.preview.audio.play();
+      vm.isPlaying = true;
     }
-    vm.isPlaying = !vm.isPlaying;
   }
 
 }
@@ -106,6 +114,45 @@ function listaMusicas($http){
     var searchState = {
       name: 'search',
       url: '/search',
+      template: `
+      <div class="co-12 topbar row">
+          <div><input name="search_bar" type="text" id="searchBar" ng-model="vm.searchBar" class="co-10" placeholder="Search for music..."></div>
+          <div type="button" class="searchButton center co-2" ng-click="vm.onSearch()">
+              <i class="glyphicon glyphicon-search searchIcon"></i></div>
+      </div>
+      <div class="center">
+          <div ng-repeat="music in vm.resultPage" ng-click="vm.playPreview(music.previewUrl)" class="co-10 alert alert-secondary result" role="alert">
+              <div class="co-2" style="min-width: 86px">
+                  <img style="border-radius: 5px; border: 1px solid #383d41" width="80px" src={{music.artworkUrl100}}>
+              </div>
+              
+              <div style="padding-top:10px" class="co-9">
+                  <h4 class="title">{{music.trackName}}</h4>
+                  <span class="duration">{{music.primaryGenreName}}</span>
+                  <br><span class="duration">{{music.kind}}</span>
+                  <br>
+                  <span class="artist">{{music.artistName}}</span>
+              </div>
+              <div class="co-1 playPreview">
+                <span ng-if="(vm.isPlaying == false) || (music.previewUrl != vm.preview.url)" style="font-size: 40px;" class="glyphicon glyphicon-play"></span>
+                <span ng-if="(vm.isPlaying == true) && (music.previewUrl == vm.preview.url)" style="font-size: 40px;" class="glyphicon glyphicon-pause"></span>
+              </div>
+          </div>
+          <div ng-if="vm.results.length == 0 || vm.results == undefined" class="co-10 result animate-if">
+              <i class="glyphicon glyphicon-exclamation-sign"></i> No results found
+          </div>
+          <br><br><br>
+          <div ng-if="vm.results.length != undefined" class="co-12">
+          <button ng-disabled="vm.currentPage == 0" ng-click="vm.previousPage()"> Previous </button>
+            Page {{vm.currentPage+1}} of {{vm.numberOfPages()}}
+          <button ng-disabled="vm.currentPage > ({{vm.numberOfPages()}}-2)" ng-click="vm.nextPage()">Next</button>
+          </div>
+      </div>`
+    }
+  
+    var resultState = {
+      name: 'result',
+      url: '/result',
       template: `
       <div class="co-12 topbar row">
           <div><input name="search_bar" type="text" id="searchBar" ng-model="vm.searchBar" class="co-10" placeholder="Search for music..."></div>
@@ -135,16 +182,6 @@ function listaMusicas($http){
           <button ng-disabled="vm.currentPage > ({{vm.numberOfPages()}}-2)" ng-click="vm.nextPage()">Next</button>
           </div>
       </div>`
-    }
-  
-    var resultState = {
-      name: 'result',
-      url: '/result',
-      template: `<main>
-      <section id="content">
-          <h3>Pagina 2</h3>
-      </section>
-  </main>`
     }
   
     $stateProvider.state(searchState);
